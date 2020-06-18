dd96d2c2743a9474a696108741720c59ef299b90
andy winarko <winarcooo@gmail.com>
2020-06-18 16:21:54 +0700

    hashtable learn

--- ice_cream_parlor/solution.py
+++ ice_cream_parlor/solution.py
@@ -1,12 +1,14 @@
-def whatFlavors(cost, money):
+def whatFlavors(costs, money):
+    table = []
+
+    for i, cost in enumerate(costs):
+        need = money - cost
+
+        if cost in table: return (table[cost], i+1)
+        
+        table[need] = i+1
+
     
-    temp = []
-
-    -
-
-
-
-
 if __name__ == '__main__':
     t = int(input())
 
@@ -16,5 +18,10 @@ if __name__ == '__main__':
         n = int(input())
 
         cost = list(map(int, input().rstrip().split()))
+
+        # m = 6
+        # ice = [1,3,4,5,6]
+
+        # result = index 1 and 4
 
         print(whatFlavors(cost, money))
